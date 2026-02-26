adc3c9ef311bc1be8bc83be258ec561363c9e391
Yomal101 <yomalbimsara2002@gmail.com>
2026-02-26 20:26:45 +0530

    Add Bookmark CRUD use-cases in application layer

---
+++ backend/src/application/usecases/Bookmark/createBookmark.js
@@ -0,0 +1,45 @@
+const Bookmark = require("../../../domain/entities/Bookmark");
+
+class CreateBookmark {
+    constructor({ bookmarkRepository }) {
+        this.bookmarkRepository = bookmarkRepository;
+    }
+
+    async execute(data) {
+
+        if (!data.materialId) {
+            throw new Error("materialId is required");
+        }
+
+        if (!data.userId) {
+            throw new Error("userId is required");
+        }
+
+        /*
+        if (!data.title || !data.title.trim()) {
+            throw new Error("Title is required");
+        }
+
+        if (!data.contentUrl || !data.contentUrl.trim()) {
+            throw new Error("Content URL is required");
+        }
+        */
+      
+    
+
+        const bookmark = new Bookmark({
+            userId: data.userId,
+            materialId: data.materialId,
+            materialTitle: data.materialTitle,
+            materialContentUrl: data.materialContentUrl,
+            note: data.note.trim()
+        });
+
+
+        return await this.bookmarkRepository.save(bookmark);
+
+    }
+
+}
+
+module.exports = CreateBookmark;
