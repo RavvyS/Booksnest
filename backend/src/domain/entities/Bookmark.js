10b0d382958aa1b391451bd03646da588ea3a1f7
Yomal101 <yomalbimsara2002@gmail.com>
2026-02-26 20:08:39 +0530

    Add Bookmark entity and persistence schema to support bookmark feature

---
+++ backend/src/domain/entities/Bookmark.js
@@ -0,0 +1,13 @@
+class Bookmark {
+    constructor({ id, userId, materialId, materialTitle, materialContentUrl, note, createdAt}) {
+        this.id = id;
+        this.userId = userId;
+        this.materialId = materialId;
+        this.materialTitle = materialTitle;
+        this.materialContentUrl = materialContentUrl;
+        this.note = note;
+        this.createdAt = createdAt || new Date();
+    }
+}
+
+module.exports = Bookmark;
---
+++ backend/src/infrastructure/database/schemas/BookmarkSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const bookmarkSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        materialId: {
+            //type: mongoose.Schema.Types.ObjectId,
+            type: String,
+            ref: 'Material',
+            required: true,
+            index: true
+        },
+        materialTitle: {
+            type: String,
+            required: true,
+            //ref: 'Material'
+        },
+        materialContentUrl: {   
+            type: String,
+            required: true,
+            //ref: 'Material'
+        },
+        note: {
+            type: String,   
+            required: false,
+            trim: true
+        }
+    },
+
+
+    {
+        timestamps: true
+    }
+);
+
+
+module.exports = mongoose.model('Bookmark', bookmarkSchema);
